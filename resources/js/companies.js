07434a3e85478f0b5a54ca712aa16170622b051c
Vlad Polovyi <vlad.polovyi@gmail.com>
2018-09-16 23:40:39 +0300

    There we go, news block have been just finished i bealive

--- resources/js/companies.js
+++ resources/js/companies.js
@@ -1,7 +1,6 @@
 "use strict";
 
 $(function() {
-
     $.ajax({
         type: 'GET',
         url: 'http://codeit.pro/codeitCandidates/serverFrontendTest/company/getList', 
@@ -21,28 +20,27 @@ $(function() {
                 $("#companiesList").append('<a  id="company' + index + '" class="list-group-item list-group-item-action">'+ value.name + '</a>');
 
                 $('#company' + index + '').click(function() {
-
-                    partnersUpdate(value.partners.sort(dynamicSort(sortOrder)), index, value);
+                    onPartnersUpdate(value.partners.sort(dynamicSort(sortOrder)), index, value);
                    
                     //if onlick sort occurs
                     $("#byNameUp").click(function(){
                         let newSortOrder = "name";
-                        partnersUpdate(value.partners.sort(dynamicSort(newSortOrder)), index, value);
+                        onPartnersUpdate(value.partners.sort(dynamicSort(newSortOrder)), index, value);
                         sortOrder = newSortOrder;
                     });
                     $("#byNameDown").click(function(){
                         let newSortOrder = "-name";
-                        partnersUpdate(value.partners.sort(dynamicSort(newSortOrder)), index, value);
+                        onPartnersUpdate(value.partners.sort(dynamicSort(newSortOrder)), index, value);
                         sortOrder = newSortOrder;
                     });
                     $("#byPersentageUp").click(function(){
                         let newSortOrder = "value";
-                        partnersUpdate(value.partners.sort(dynamicSort(newSortOrder)), index, value);
+                        onPartnersUpdate(value.partners.sort(dynamicSort(newSortOrder)), index, value);
                         sortOrder = newSortOrder;
                     });
                     $("#byPersentageDown").click(function(){
                         let newSortOrder = "-value";
-                        partnersUpdate(value.partners.sort(dynamicSort(newSortOrder)), index, value);
+                        onPartnersUpdate(value.partners.sort(dynamicSort(newSortOrder)), index, value);
                         sortOrder = newSortOrder;
                     });
 
@@ -69,13 +67,9 @@ $(function() {
             $(".list-group .list-group-item").click(function(e) {
                 $(".list-group .list-group-item").removeClass("active");
                 $(e.target).addClass("active");
-             });
-
-        }, 0);
+            });
+        }, 500);
     });
-
-
-
 
     $.ajax({
         type: 'GET',
@@ -83,22 +77,41 @@ $(function() {
         dataType: 'json', 
         encode: true
     }).done(function(data) {
+
+        $.each(data.list, function (index, value) {
+            let description = value.description;
+            if (description.length > 90) {
+                description = description.substring(0, 90) + "...";
+            }
+            
+            let slide = 
+            `<div id="slide`+ index +`" class="carousel-item">
+                <img class="image" src="`+value.img+`" alt="img`+index+`">
+                <div class="carousel-caption text-dark">
+                    <h6>`+value.link+`</h6>
+                    <p>`+description+`</p>
+                    <p><b>`+value.author+`</b></p>
+                    <p>Posted: `+getDate(value.date)+`</p>
+                </div>
+            </div>`,
+            indicator = `<li data-target="#demo" data-slide-to="`+index+`"></li>`;
+    
+            $("#slideshow").append(slide);
+            $("#indicators").append(indicator);
+        });
+        $("#slide0").addClass("active");
+        $("#loader4").css("display", "none");
+
         console.log(data);
     });
 
-
-
-
-
-
 });
 
-function partnersUpdate(partners, index, value) {
+function onPartnersUpdate(partners, index, value) {
     $("#companyPartnersList").empty();
 
     //display list of partners for each company
     $.each(value.partners, function (index, value) {
-
         let progress =
         `<div class="progress progress-bar-vertical flex-item">
             <div class="progress-bar bg-info" role="progressbar" style="height:`+ value.value +`%;" ></div>
@@ -119,4 +132,21 @@ function dynamicSort(property) {
         var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
         return result * sortOrder;
     }
+}
+
+function getDate(date) {
+    var a = new Date(date * 1000);
+    var year = a.getFullYear();
+    var month = a.getMonth();
+    var day = a.getDate();
+    
+    if(day < 10) {
+        day = "0" + day;
+    }
+    if(month < 10) {
+        month = "0" + month;
+    }
+    var time = day + '.' + month + '.' + year;
+
+    return time;
 }
--- resources/js/register.js
+++ resources/js/register.js
@@ -1,9 +1,7 @@
 "use strict";
 
 $(function() {
-    
     $('form').submit(function(event) {
-
         var formData = {
             name: $('input[name=name]').val(),
             secondname: $('input[name=secondname]').val(),
@@ -20,18 +18,18 @@ $(function() {
             dataType: 'json', 
             encode: true
         }).done(function(data) {
-
             if(data.status === 'OK') {
-                $("#message").empty().append("User has been created");
+                $("#message").empty().append('<div class="alert alert-success" role="alert">User has been successfully created. Redirecting...</div>');
                 setTimeout( function() {
                     $(location).attr('href', './companies.html');
                 }, 2000);
-            } else if(data.status === 'Form error' || data.status === 'Error') {
-                $("#message").empty().append(data.message);
+            } else if(data.status === 'Error') {
+                $("#message").empty().append('<div class="alert alert-danger" role="alert">'+ data.message +'</div>');
+                console.log(data);
             }else {
-                $("#message").empty().append("Woops, something went wrong. Unknown error occured, please try something else");
+                $("#message").empty().append('<div class="alert alert-warning" role="alert">Woops, something went wrong</div>');
             }
-        });
+        }); 
 
         // stop the form from submitting the normal way and refreshing the page
         event.preventDefault();
